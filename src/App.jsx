1639428c60cbfd385a41df79d3e6c896d4b04a9b
godfather <javadgeravand96@gmail.com>
2023-10-22 17:40:30 +0330

    add routes and temlpate of BlogPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
 import HomePage from "./components/Home/HomePage";
+import Blogs from "./components/blog/Blogs";
+import BlogPage from "./components/blog/BlogPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <HomePage />
+   <BrowserRouter>
+   <Routes>
+      <Route path="/blog/:id" element={<BlogPage/>}></Route>
+      <Route path="/blogs" element={<Blogs/>}></Route>
+      <Route path="/" element={<HomePage/>}></Route>
+    </Routes>
+   </BrowserRouter>
     </>
   )
 }
