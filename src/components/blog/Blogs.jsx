c020eba5cb9ba02bc3b5d38fc85545eb7c035364
Narges Makvandi <narges.makvandi@yahoo.com>
2023-10-22 16:33:41 +0330

    ceate cardEl

--- src/components/blog/Blogs.jsx
+++ src/components/blog/Blogs.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from '@apollo/client'
-import React from 'react'
 import GET_BLOGS_INTO from '../../graphql/queries'
+import CardEl from '../shared/CardEl'
 
 const Blogs = () => {
-    const {loading,data,errors} = useQuery(GET_BLOGS_INTO)
+    const {data} = useQuery(GET_BLOGS_INTO)
+
   return (
     <div>
       {data && data.posts && data.posts.data.map((post) => (
-            <h1 >{post.title}</h1>
-
-        ))}
+          <CardEl key={post.id} {...post} />
+      ))}
     </div>
   )
 }
