459e1610fe83ffd32d03d4bdc93eaf1fcaed2c7a
Narges Makvandi <narges.makvandi@yahoo.com>
2023-11-01 13:57:10 +0330

    set Api

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useQuery } from "@apollo/client"
+// import { useParams } from "react-router-dom"
+import { GET_BLOG_INTO } from "../../graphql/queries"
 
 const BlogPage = () => {
+
+  // const { id } = useParams()
+
+  const { data } = useQuery(GET_BLOG_INTO)
+
+  console.log(data)
+
   return (
     <div>BlogPage</div>
   )
