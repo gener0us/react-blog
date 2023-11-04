faf7964a9af5e36a3171a422005a91b985d1b200
Narges Makvandi <narges.makvandi@yahoo.com>
2023-11-04 16:06:33 +0330

    create contact form for Add User

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import HomePage from "./components/Home/HomePage";
-import Blogs from "./components/blog/Blogs";
-import BlogPage from "./components/blog/BlogPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Home/HomePage"
+import Blogs from "./components/blog/Blogs"
+import BlogPage from "./components/blog/BlogPage"
+import Contact from "./components/contact/Contact"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
     <>
    <BrowserRouter>
-   <Routes>
+    <Routes>
       <Route path="/blogs/:id" element={<BlogPage/>}></Route>
       <Route path="/blogs" element={<Blogs/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
       <Route path="/" element={<HomePage/>}></Route>
     </Routes>
    </BrowserRouter>
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react"
+
+const Contact = () => {
+
+  const [contact, setContact] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    phone: ''
+  })
+
+  const changeHandler = (event) => {
+    const name = event.target.name
+    const value = event.target.value 
+
+    setContact((contact) => ({...contact, [name]: value}))
+  }
+
+  const AddHandler = () => {
+    console.log(contact)
+  }
+
+  return (
+    <div className="container">
+      <div className="text-center">
+        <h1 className="font-black text-4xl pt-12 pb-2 text-purple-800"> Contact App </h1>
+        <p className="font-semibold text-lg text-purple-900"> Lorem ipsum | React js Full Course </p>
+      </div>
+      <form className="p-5 bg-white shadow-xl shadow-purple-300 mt-10 rounded-xl">
+        <div className="grid grid-cols-2 gap-3">
+          <div>
+            <label 
+              htmlFor="first_name" 
+              className="block mb-2 text-sm font-medium text-gray-900">
+                name
+            </label>
+            <input 
+              type="text" 
+              id="first_name" 
+              className="input-style" 
+              placeholder="name"
+              name="name"
+              onChange={changeHandler}
+              value={contact.name} />
+          </div>
+          <div>
+            <label 
+              htmlFor="last_name" 
+              className="block mb-2 text-sm font-medium text-gray-900"> 
+              last name
+            </label>
+            <input 
+              type="text" 
+              id="last_name" 
+              className="input-style" 
+              placeholder="lastname" 
+              name="lastname"
+              onChange={changeHandler}
+              value={contact.lastname} />
+          </div>
+          <div>
+            <label 
+              htmlFor="email" 
+              className="block mb-2 text-sm font-medium text-gray-900">
+                email
+            </label>
+            <input 
+              type="email" 
+              id="email" 
+              className="input-style" 
+              placeholder="email"
+              name="email"
+              onChange={changeHandler}
+              value={contact.email} />
+          </div>
+          <div>
+            <label 
+              htmlFor="phome" 
+              className="block mb-2 text-sm font-medium text-gray-900"> 
+              phone number 
+            </label>
+            <input 
+              type="text" 
+              id="phome" 
+              className="input-style" 
+              placeholder="phone number" 
+              name="phone"
+              onChange={changeHandler}
+              value={contact.phone} />
+          </div> 
+        </div>
+        <button onClick={AddHandler} type="submit" className="btn-primary mt-5">Add User</button>      
+      </form>
+    </div>
+  )
+}
+
+export default Contact
